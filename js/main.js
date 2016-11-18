22dfe0c47ea52de6484d2501835e528579496ec4
Marcin-Kurpiewski <kalinka123A>
2016-11-18 17:23:31 +0100

    refactor images add js animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+setInterval(function randomAnimation(){
+    var randomDIV= Math.floor(Math.random()*8);
+    var element= document.getElementsByClassName('portfolio-item');
+    var typeAnimations=
+        ["bounce",
+        "flash",
+        "pulse",
+        "shake",
+        "headShake",
+        "swing",
+        "tada",
+        "wobble",
+        "jello"];
+    var randomAnimation= typeAnimations[Math.floor(Math.random()*9)];
+    var newElement= element[randomDIV];
+    newElement.className='portfolio-item animated ' + randomAnimation;
+},3000);
